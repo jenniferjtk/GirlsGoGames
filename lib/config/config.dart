e79dc4e506d70da4ef5571607a94db1958eec594
jenniferjtk <jennagracejohnson3@gmail.com>
2026-06-24 22:03:58 -0400

    Import selected ReadRight codebase with Supabase migration to team backend

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -2,7 +2,6 @@ class AppConfig {
   static const primaryColor = 0xFF6BD425;   // light green
   static const secondaryColor = 0xFF4DB6E2; // light blue
   static const appName = "ReadRight";
-  static const supabaseUrl = 'https://byhmgdgjlyphwyilrfjm.supabase.co';
-  static const supabaseAnonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ5aG1nZGdqbHlwaHd5aWxyZmptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE4NDU1NDksImV4cCI6MjA3NzQyMTU0OX0.rkxUJIWYpoPpCV3azuK7vwenPATJeLjzTdTn13savZM';
+  static const supabaseUrl = 'https://fdqfxkddijsbbhwgmfeu.supabase.co';
+  static const supabaseAnonKey = 'sb_publishable_GIVWSnd43r2In0rM6VIo3Q_KJk0tR_H';
 }
